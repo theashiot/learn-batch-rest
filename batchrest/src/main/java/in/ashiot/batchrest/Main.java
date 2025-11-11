0ce0649d0635538f43a6e8a17da56c0ff3584c44
Ashwin Mehendale <ashness@individuality.in>
2025-11-11 12:13:54 +0530

    parse json output

--- batchrest/src/main/java/in/ashiot/batchrest/Main.java
+++ batchrest/src/main/java/in/ashiot/batchrest/Main.java
@@ -4,36 +4,73 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
 
 /**
  *
  * <p>
  * The servlet is registered and mapped to /HelloServlet using the {@linkplain WebServlet
  */
-@WebServlet("/HelloWorld")
+@WebServlet("/report")
 public class Main extends HttpServlet {
 
-    static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
-
+    private static final Client CLIENT = ClientBuilder.newClient();
+    static String PAGE_HEADER = "<html><head><title>GitHub Report</title></head><body>";
     static String PAGE_FOOTER = "</body></html>";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+             
+        
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>Hello World!</h1>");
         
         GitRest gitRest = new GitRest();
 
-        String msg = gitRest.getIssues();
+        try {
+                String msg = gitRest.getIssues(CLIENT);
+                IssueController isc = new IssueController();
+                isc.inferIssuesFromGHResponse(msg);                
+                writer.println("<h1>Report of the wildfly-core repository</h1>");
+                int noOfIssues = isc.getIssueCount();
+                writer.println("<p>Found "+noOfIssues+" issues.</p>");
+                List <GitIssue> issues;
+                
+                issues = isc.getGitIssueList();
 
-        writer.println("<h2>"+msg+"</h2>");
-        
-        writer.println(PAGE_FOOTER);
-        writer.close();
+                writer.println("<table border= \"1\" style=\"width: 100%;\"> "+
+                               "<tr>" +
+                               "<th>Issue ID</th>" +
+                               "<th>Title</th>" +
+                               "<th>Creator</th>" +
+                               "<th>Last Update</th>" +
+                               "</tr>");
+                for (GitIssue issue : issues) {
+                    writer.println("<tr>");
+                    writer.println("<td>"+ issue.getIssueID()+"</td>");
+                    writer.println("<td>"+ issue.getTitle()+"</td>");
+                    writer.println("<td>"+ issue.getCreator()+"</td>");
+                    writer.println("<td>"+ issue.getLastUpdate()+"</td>");
+                    writer.println("</tr>");
+                }
+                writer.println("</table>");
+                
+                writer.println(PAGE_FOOTER);
+                writer.close();
+        } finally {
+        }
+
+    }
+
+    public void destroy () {
+        CLIENT.close();
     }
 
 }
